a8c6ef8c1dd4f1250a4e740d2ccfa05c12d300df
Anneliese Kappey <108365625+anneliesekappey@users.noreply.github.com>
2023-04-29 07:48:01 +0200

    finish learn more page

--- src/components/ui/LearnMore.js
+++ src/components/ui/LearnMore.js
@@ -11,8 +11,6 @@ import { ArrowBack } from '@mui/icons-material'
 import { Hidden } from '@mui/material'
 import { useMediaQuery } from '@mui/material'
 import { Button } from '@mui/material'
-// import ManBackReal from '../../images/screens/ManBackReal.jpg'
-// import ManSideReal from '../../images/screens/ManSideReal.jpg'
 import { Box } from '@mui/material'
 import { Paper } from '@mui/material'
 import { MobileStepper } from '@mui/material'
@@ -20,100 +18,62 @@ import { useTheme } from '@emotion/react'
 
 const testimonials = [
   {
-    label: 'Dear Mary Ellen and Susan',
-    description: `I would like to take this time to
+    label: 'Testimonial from  ~Carolyn Kokish',
+    description: `"Dear Mary Ellen and Susan ~~~ I would like to take this time to
         acknowledge both of you for the incredible job you are doing with
         your Virtual Gatherings group. Arthur, who is diagnosed with
-        moderate dementia , is thrilled to participate every Saturday.This
-        disease has transformed him in his daily life from a very verbal ,
-        interested , intelligent , social , engaged person to a very quiet
-        , introverted , apathetic person with cognitive impairments. My
-        heart breaks to watch him just manage in his every day activities.
+        moderate dementia, is thrilled to participate every Saturday. This
+        disease has transformed him in his daily life from a very verbal,
+        interested, intelligent, social, engaged person to a very quiet, introverted, apathetic person with cognitive impairments. My
+        heart breaks to watch him just manage in his everyday activities.
         However, on Saturdays he miraculously becomes excited, happy, more
-        verbal , more alert and extroverted. He loves your program and as
+        verbal, more alert and extroverted. He loves your program and as
         I pass by I can hear him fully involved and engaged in your
         various activities. I have him enrolled in other programs through
-        the Alzheimer’s Association ,NYU and the VA. All provide some form
-        of stimulation but none get it as “ right” as both of you. Your
+        the Alzheimer’s Association, NYU and the VA. All provide some form
+        of stimulation but none get it as “right” as both of you. Your
         activities are so appropriate for his limitations yet help him to
         feel good about himself and at the same time give him enormous
         pleasure. This type of stimulation is priceless. I thank you both
         from the bottom of my heart. It also helps me as a caregiver to
         feel more lighthearted about his disease. Your ways of making them
         felt better about themselves transfers to the caregivers who then
-        feel better about the situation. Again I Thank you,`,
+        feel better about the situation. Again, I thank you."`,
   },
   {
-    label: 'SECOND TEST',
-    description: `I would like to take this time to
-        acknowledge both of you for the incredible job you are doing with
-        your Virtual Gatherings group. Arthur, who is diagnosed with
-        moderate dementia , is thrilled to participate every Saturday.This
-        disease has transformed him in his daily life from a very verbal ,
-        interested , intelligent , social , engaged person to a very quiet
-        , introverted , apathetic person with cognitive impairments. My
-        heart breaks to watch him just manage in his every day activities.
-        However, on Saturdays he miraculously becomes excited, happy, more
-        verbal , more alert and extroverted. He loves your program and as
-        I pass by I can hear him fully involved and engaged in your
-        various activities. I have him enrolled in other programs through
-        the Alzheimer’s Association ,NYU and the VA. All provide some form
-        of stimulation but none get it as “ right” as both of you. Your
-        activities are so appropriate for his limitations yet help him to
-        feel good about himself and at the same time give him enormous
-        pleasure. This type of stimulation is priceless. I thank you both
-        from the bottom of my heart. It also helps me as a caregiver to
-        feel more lighthearted about his disease. Your ways of making them
-        felt better about themselves transfers to the caregivers who then
-        feel better about the situation. Again I Thank you,`,
+    label: 'Testimonial from  ~Kathryn Green',
+    description: `"Thank you, Virtual Gatherings, for your amazing personalized social engagement with my husband, Carl, and others diagnosed with dementia. I truly appreciate the special care that Mary Ellen, Susan, and Jenn have taken to know each participant – their hobbies, careers, and likes – to create stimulating activities every week such as singing, reading, exercises (in the chair), memory games, even sign language. For Carl’s birthday, they exceeded all expectations and created an amazing experience by singing Happy Birthday, sharing a virtual card, and sharing a pictorial slideshow of memories from Carl’s life. Every participant individually gave best wishes to Carl. It created an impact and such joy to kick off his birthday weekend. Each week Carl looks forward to joining his friends on Zoom and I’m delighted that he has these activities. In addition to giving Carl such amazing experiences, I’ve enjoyed participating in the caregiver sessions. In the last year, Virtual Gatherings created opportunities to talk with professionals about recent research and treatments in dementia. We also had sing-a-longs with our partner; prior to this, Carl rarely sang in public! While dementia can be difficult, Virtual Gatherings provides a happy and hopeful environment for everyone. I’m so glad that we found you!" 
+    `,
   },
   {
-    label: 'THIRD TEST',
-    description: `I would like to take this time to
-        acknowledge both of you for the incredible job you are doing with
-        your Virtual Gatherings group. Arthur, who is diagnosed with
-        moderate dementia , is thrilled to participate every Saturday.This
-        disease has transformed him in his daily life from a very verbal ,
-        interested , intelligent , social , engaged person to a very quiet
-        , introverted , apathetic person with cognitive impairments. My
-        heart breaks to watch him just manage in his every day activities.
-        However, on Saturdays he miraculously becomes excited, happy, more
-        verbal , more alert and extroverted. He loves your program and as
-        I pass by I can hear him fully involved and engaged in your
-        various activities. I have him enrolled in other programs through
-        the Alzheimer’s Association ,NYU and the VA. All provide some form
-        of stimulation but none get it as “ right” as both of you. Your
-        activities are so appropriate for his limitations yet help him to
-        feel good about himself and at the same time give him enormous
-        pleasure. This type of stimulation is priceless. I thank you both
-        from the bottom of my heart. It also helps me as a caregiver to
-        feel more lighthearted about his disease. Your ways of making them
-        felt better about themselves transfers to the caregivers who then
-        feel better about the situation. Again I Thank you,`,
+    label: 'Testimonial from  ~Eileen',
+    description: `"Dear Susan and Mary Ellen ~~~ I can’t thank you enough for Paul’s birthday celebration. It was so thoughtfully and creatively put together. In general, virtual Gatherings makes all the difference for Paul — giving him a community and ability to engage. I thank you from the bottom of my heart for doing all you do to make Paul happy. With gratitude."`,
   },
   {
-    label: 'FOURTH TEST',
-    description: `I would like to take this time to
-        acknowledge both of you for the incredible job you are doing with
-        your Virtual Gatherings group. Arthur, who is diagnosed with
-        moderate dementia , is thrilled to participate every Saturday.This
-        disease has transformed him in his daily life from a very verbal ,
-        interested , intelligent , social , engaged person to a very quiet
-        , introverted , apathetic person with cognitive impairments. My
-        heart breaks to watch him just manage in his every day activities.
-        However, on Saturdays he miraculously becomes excited, happy, more
-        verbal , more alert and extroverted. He loves your program and as
-        I pass by I can hear him fully involved and engaged in your
-        various activities. I have him enrolled in other programs through
-        the Alzheimer’s Association ,NYU and the VA. All provide some form
-        of stimulation but none get it as “ right” as both of you. Your
-        activities are so appropriate for his limitations yet help him to
-        feel good about himself and at the same time give him enormous
-        pleasure. This type of stimulation is priceless. I thank you both
-        from the bottom of my heart. It also helps me as a caregiver to
-        feel more lighthearted about his disease. Your ways of making them
-        felt better about themselves transfers to the caregivers who then
-        feel better about the situation. Again I Thank you,`,
+    label:
+      'Testimonial from  ~Mariya Rivkin, MBA Head of Operations Isaac Health',
+    description: `"Hi Mary Ellen & Susan ~~~ We wanted to follow up and let you know how wonderful it was to watch the session today. We were so impressed with the variety of the programming and how well your team kept the conversation going in a virtual setting and with such different personalities and levels of engagement/cognitive ability. From yoga to word games to sharing what they were grateful for, it was really heartwarming to see how they all felt like they were part of the community. It was also great to see that your team knew so much about each participant's life and that they were all comfortable enough to share personal details with the group and even sing some Frank Sinatra in front of everyone. The fact that everyone mentioned that they were grateful for the virtual gatherings community is a real testament to the work your team does. Thank you again for inviting us and we are looking forward to our future event collaboration!"
+    `,
+  },
+  {
+    label: 'Testimonial from  ~Judy',
+    description: `"Dear Susan and Mary Ellen ~~~ Today’s anniversary tribute to Jackie and Milt was spectacular. It made the day very special — not an easy feat in these times. Thank you, thank you. We all had a great time."`,
+  },
+  {
+    label: 'Testimonial from  ~Henry Baker',
+    description: `"Dear Susan and Mary Ellen ~~~ I think the program has been an enormous success for Liz. I’ve noticed an improvement in her communication skills, and so have others. But more importantly, you both have enriched her life more than you probably know. I always park myself nearby in the event of “technical issues,” so I hear the laughter, and the music, and the other members of the group, telling their stories and making their comments. Liz feels so at home home with everyone now! She laughs a lot! I think one of the reasons that this has been so much fun and so successful is that you have devised a format that provides the group with consistency, and at the same time an amazing amount of variety. I admire you for thinking up all the things that go into each session: the music, the games, the conversations, the exercises, the observances of holidays and birthdays, discussions of current events……..all of it! I credit our son with discovering Gatherings, and I’m so glad he did! It’s been a wonderful thing for Liz, and she’s so lucky to be among such kind, thoughtful, and smart kindred spirits. Thank you! Thank you! Thank you!"`,
+  },
+  {
+    label: 'Testimonial from  ~Judy',
+    description: `"Mary Ellen and Susan have created a thoughtful, meaningful and personalized program for memory-impaired individuals. Mom attends these Zoom events twice weekly and is engaged and energized by the word games, movement activities, music, art and discussion. Much of the program is interactive and these two-hour sessions are stimulating and nurturing for both the participants and their caregivers."`,
+  },
+  {
+    label: 'Testimonial from  ~Gary Lewis',
+    description: `"The Gatherings sessions are the highlights of Carole’s week. She looks forward to them very much, loves the people and feel they are all friends."`,
+  },
+  {
+    label: 'Testimonial from  ~Susan',
+    description: `"Dear Mary Ellen and Susan ~~~ Thank you so much for the lovely birthday celebration for Bob. You both really put a lot of effort into organizing a very personal event and he really was quite pleased. Sorry I may have grabbed more time than you expected and you had more prepared. I hope you know that you've made life so much more livable during Covid than it might have been for us. Best wishes."`,
   },
 ]
 
@@ -166,15 +126,15 @@ const LearnMore = () => {
         </Hidden>
         <Grid container direction="row" justifyContent={'center'}>
           <Grid item>
-            <Box sx={{ maxWidth: 600, flexGrow: 1 }}>
+            <Box sx={{ maxWidth: 700, flexGrow: 1 }}>
               <Paper
                 square
                 elevation={15}
                 sx={{
                   display: 'flex',
                   alignItems: 'center',
-                  height: 80,
-                  pl: 2,
+                  height: 70,
+                  padding: 3,
                   bgcolor: '#FAF9F6',
                   color: '#FE5F55',
                   borderRadius: 1,
@@ -193,10 +153,11 @@ const LearnMore = () => {
               </Paper>
               <Box
                 sx={{
-                  height: 390,
-                  maxWidth: 600,
-                  //   width: '100%',
-                  p: 2,
+                  height: 410,
+                  maxWidth: 700,
+                  width: '100%',
+                  paddingTop: 1.5,
+                  paddingBottom: 5,
                 }}
               >
                 <Paper
@@ -205,8 +166,11 @@ const LearnMore = () => {
                   sx={{
                     display: 'flex',
                     alignItems: 'center',
-                    height: 390,
-                    pl: 2,
+                    height: 410,
+                    paddingLeft: 3,
+                    paddingRight: 3,
+                    paddingTop: 0.4,
+                    paddingBottom: 0.4,
                     bgcolor: '#FAF9F6',
                     color: '#868686',
                     borderRadius: 2,
@@ -215,8 +179,11 @@ const LearnMore = () => {
                   <Typography
                     sx={{
                       fontFamily: 'Cardo',
-                      lineHeight: '1.2em',
+                      fontWeight: 'light',
+                      fontStyle: 'italic',
+                      lineHeight: '1.3em',
                       fontSize: '1.1em',
+                      textAlign: 'justify',
                     }}
                   >
                     {testimonials[activeStep].description}
@@ -260,42 +227,6 @@ const LearnMore = () => {
             </Box>
           </Grid>
         </Grid>
-        {/* <Grid
-            container
-            display={'flex'}
-            flexDirection="row"
-            spacing={2}
-            justifyContent={'space-around'}
-            sx={{ marginTop: '1.5em', marginLeft: '1.5em' }}
-          >
-            <Typography variant="h3">Testimonials</Typography>
-            <img src={ManBackReal} alt="" width={320} height={240} />
-            <img src={ManSideReal} alt="" width={320} height={240} />
-            <Typography variant="p">
-              Dear Mary Ellen and Susan, I would like to take this time to
-              acknowledge both of you for the incredible job you are doing with
-              your Virtual Gatherings group. Arthur, who is diagnosed with
-              moderate dementia , is thrilled to participate every Saturday.This
-              disease has transformed him in his daily life from a very verbal ,
-              interested , intelligent , social , engaged person to a very quiet
-              , introverted , apathetic person with cognitive impairments. My
-              heart breaks to watch him just manage in his every day activities.
-              However, on Saturdays he miraculously becomes excited, happy, more
-              verbal , more alert and extroverted. He loves your program and as
-              I pass by I can hear him fully involved and engaged in your
-              various activities. I have him enrolled in other programs through
-              the Alzheimer’s Association ,NYU and the VA. All provide some form
-              of stimulation but none get it as “ right” as both of you. Your
-              activities are so appropriate for his limitations yet help him to
-              feel good about himself and at the same time give him enormous
-              pleasure. This type of stimulation is priceless. I thank you both
-              from the bottom of my heart. It also helps me as a caregiver to
-              feel more lighthearted about his disease. Your ways of making them
-              felt better about themselves transfers to the caregivers who then
-              feel better about the situation. Again I Thank you,
-            </Typography>
-          </Grid>
-        </Grid> */}
         <Grid
           container
           direction="column"
